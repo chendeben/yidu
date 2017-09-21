19c30d0f2552da82c70d89d1fa45bd2e5893b65f
chendeben <cdb@meitu.com>
2017-09-21 14:57:06 +0800

    fix

--- yidu/yispider/start.sh
+++ yidu/yispider/start.sh
@@ -4,5 +4,4 @@
 #export PATH=$PATH:$JAVA_HOME/bin
 #export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar
 
-#/usr/java/jdk1.6.0_45/bin/java -jar spider.jar -ca &
-/usr/java/jdk1.6.0_45/bin/java -jar spider.jar -ca &
+java -jar spider.jar -ca &
